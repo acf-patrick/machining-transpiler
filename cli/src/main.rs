01fa2088754d63fa0924bb13bc891ef4d63200fc
acf-patrick <miharisoap@gmail.com>
2024-03-15 17:42:09 +0300

    setup CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    /// Name of the project to export
+    #[arg(short = 'n', long)]
+    project_name: Option<String>,
+
+    /// UUID of the project to export
+    #[arg(short = 'u', long)]
+    project_uuid: Option<String>,
+
+    /// ID of the project to export
+    #[arg(short = 'i', long)]
+    project_id: Option<u32>,
+
+    /// Name of the provider (Elumatec, ...)
+    #[arg(short, long)]
+    vendor: Option<String>,
+
+    /// Path to output file
+    #[arg(short, long)]
+    output: Option<String>,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List implemented providers
+    Vendors,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    todo!()
 }
